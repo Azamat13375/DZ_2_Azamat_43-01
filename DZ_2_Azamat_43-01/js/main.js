d7eec41a359afd9113fd8c2079e0af60246d5c07
Azamat13375 <cerambit5@gmail.com>
2024-11-07 01:00:04 +0500

    Add files via upload

---
+++ DZ_2_Azamat_43-01/js/main.js
@@ -0,0 +1,115 @@
+// RANDOM COLOR GENERATOR
+
+const buttonsColor = document.querySelectorAll('.btn-color')
+const javaScript = document.querySelector('#js-color')
+
+const generateRandomColor = () => {
+    const hexCodes = '0123456789ABCDEF'
+    let color = ''
+    for (let i = 0; i < 6; i++) {
+        color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
+    }
+    return '#' + color
+}
+
+const setRandomColors = () => {
+    buttonsColor.forEach((buttonColor) => {
+        buttonColor.innerHTML = generateRandomColor()
+        buttonColor.onclick = (event) => {
+            javaScript.style.color = event.target.innerHTML
+        }
+    })
+}
+
+window.onload = () => setRandomColors()
+window.onkeydown = (event) => {
+    if (event.code.toLowerCase() === 'space') {
+        event.preventDefault()
+        setRandomColors()
+    }
+}
+
+// SLIDER BLOCK
+
+const slides = document.querySelectorAll('.slide')
+const next = document.querySelector('#next')
+const prev = document.querySelector('#prev')
+let index = 0
+
+const hideSlide = () => {
+    slides.forEach((slide) => {
+        slide.style.opacity = 0
+        slide.classList.remove('active_slide')
+    })
+}
+const showSlide = (i = 0) => {
+    slides[i].style.opacity = 1
+    slides[i].classList.add('active_slide')
+}
+
+hideSlide()
+showSlide(index)
+
+
+const autoSlider = (i = 0) => {
+    setInterval(() => {
+        i++
+        if (i > slides.length - 1) {
+            i = 0
+        }
+        hideSlide()
+        showSlide(i)
+    }, 10000)
+}
+
+next.onclick = () => {
+    index < slides.length - 1 ? index++ : index = 0
+    hideSlide()
+    showSlide(index)
+}
+
+prev.onclick = () => {
+    index > 0 ? index-- : index = slides.length - 1
+    hideSlide()
+    showSlide(index)
+}
+
+autoSlider(index)
+
+// Основы асинхронности
+
+// Сихронный код
+
+// Асинхронный код
+
+// setTimeout( () => {
+//     console.log(1)
+// }, 2000)
+//
+// setTimeout( () => {
+//     console.log(2)
+// })
+//
+// console.log(3)
+
+// const interval = setInterval(() => {
+//     console.log('ok')
+// }, 1000)
+//
+// setTimeout( () => {
+//     clearInterval(interval)
+// }, 5000)
+
+// const button = document.querySelector('button')
+//
+// button.onclick = () => {
+//     setTimeout( () => {
+//         console.log(1)
+//     }, 1000)
+// }
+//
+// console.log(2)
+
+//Event Loop - Цикл событий
+
+// FireFox --> Qun
